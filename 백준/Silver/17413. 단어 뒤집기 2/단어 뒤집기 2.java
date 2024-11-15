958656db2e12efc157e8675497687ab4ab0c17bc
hsilnam <hsilnam3@gmail.com>
2024-11-15 17:35:33 +0900

    Update 단어 뒤집기 2.java

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -2,7 +2,12 @@ import java.io.*;
 import java.util.*;
 
 /*
-
+문자열의 문자 하나씩 검사하면서 단어('a'-'z', '0'-'9'로 이루어진 문자)만 뒤집기
+- 문자 뒤집기: Stack 자료구조를 사용
+- 현재 문자가 '<'인 경우: '>'가 나올 때까지 그대로 문자열에 추가
+- 현재 문자가 단어에 해당하지 않는 경우: 그대로 문자열에 추가
+- 현재 문자 단어에 속하는 문자인 경우 ('a'-'z', '0'-'9'): 단어 정보 쌓기
+- 단어가 끝나는 지점 / 끝까지 도달한 경우: 단어 reverse하고 문자열에 추가 && 단어 정보 초기화
  */
 
 public class Main {
@@ -14,17 +19,19 @@ public class Main {
         char[] chars = br.readLine().toCharArray();
 
         StringBuilder answer = new StringBuilder();
-        StringBuilder word = new StringBuilder();
+        Stack<Character> wordStack = new Stack<>();
         int i = 0;
         while (i < chars.length) {
             char c = chars[i];
             if (isCharForWord(c)) {
-                word.append(c);
+                wordStack.push(c);
             }
             if ((i == chars.length - 1) // 마지막에 도달한 경우
-                    || (i + 1 < chars.length - 1 && !(isCharForWord(chars[i + 1])))) { // 다음 글자가 문자에 속하는 글자가 아닌 경우
-                answer.append(word.reverse());
-                word = new StringBuilder();
+                    || (i + 1 < chars.length - 1 && !(isCharForWord(chars[i + 1])))) { // 다음 문자가 단어에 속하지 않는 경우
+                while (!wordStack.isEmpty()) {
+                    answer.append(wordStack.pop());
+                }
+                wordStack.clear();
             }
 
             if (c == '<') { // tag
