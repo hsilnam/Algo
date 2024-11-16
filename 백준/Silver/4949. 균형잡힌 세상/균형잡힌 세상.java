ffcfe4f725f8a21f4dc94184a48771e86568a174
hsilnam <hsilnam3@gmail.com>
2024-11-16 11:16:37 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 224 ms, Memory: 20028 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,51 +1,60 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 /*
-- 스택, 문자열 문제
+균형잡힌 괄호
+- 괄호(()[])들이 Stack을 통해 서로의 쌍을 남는 것 없이 매칭 시켜주면 됨
+- stack에 추가하는 조건: ([이 들어올 경우
+- stack에서 삭제하는 조건: 현재 비교하는 값이 stack의 마지막 값의 페어와 일치하는 경우
+- 에러 조건: 빈 stack에 )]이 들어오거나 )]의 쌍이 안맞을 경우
  */
+
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringBuilder result = new StringBuilder();
-        while (true) {
-            String temp = br.readLine();
-            if(temp.equals(".")) {
+        String input;
+        StringBuilder answer = new StringBuilder();
+        while ((input = br.readLine()) != null) {
+            if(input.equals(".")) {
                 break;
             }
             Stack<Character> stack = new Stack<>();
-            boolean isOk = true;
-            for (char c : temp.toCharArray()) {
-                if (!isOk && c != '.') {
+            for (int i = 0; i < input.length(); i++) {
+                char c = input.charAt(i);
+                if (!(c == '(' || c == ')' || c == '[' || c == ']')) {
                     continue;
                 }
 
                 if (c == '(' || c == '[') {
                     stack.push(c);
-                } else if (c == '.') {
-                    result.append((stack.isEmpty() && isOk) ? "yes" : "no").append("\n");
-                } else if (c == ')' || c == ']') {
-                    if (stack.isEmpty()) {
-                        isOk = false;
-                        continue;
-                    }
-                    if ((c == ')' && stack.peek() == '(') ||
-                            (c == ']' && stack.peek() == '[')) {
-                        stack.pop();
-                    } else {
-                        isOk = false;
-                    }
+                }
 
+                if (!stack.isEmpty()
+                        && ((stack.peek() == '(' && c == ')') || (stack.peek() == '[' && c == ']'))) {
+                    stack.pop();
+                    continue;
+                }
+
+                if (stack.isEmpty()
+                        && (c == ')' || c == ']')) {
+                    stack.push('e'); //err
+                    break;
+                }
+                if (!stack.isEmpty()
+                        && ((stack.peek() == '(' && c == ']') || (stack.peek() == '[' && c == ')'))) {
+                    stack.push('e'); //err
+                    break;
                 }
             }
+
+            answer.append((stack.isEmpty()) ? "yes" : "no").append("\n");
         }
 
-        bw.write(result.toString());
+        bw.write(answer.toString());
 
         br.close();
         bw.close();
     }
-
 }
