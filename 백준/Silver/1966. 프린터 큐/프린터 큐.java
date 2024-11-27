4c7336566557cf3605418db0f84bdee3b8f9d845
hsilnam <hsilnam3@gmail.com>
2024-11-27 17:04:21 +0900

    [Silver III] Title: 프린터 큐, Time: 128 ms, Memory: 15180 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,15 +1,20 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 /*
-- queue 이용
-- 몇번째 원소의 숫자였는지 기억하기 위해 idx 번호 같이 저장해주기
-- 첫번째 값과 뒤의 값들을 한바퀴 돌려서 중요도를 비교한다
-    - 하나라도 첫번째 값보다 높은게 있으면, 검사를 멈추고 뒤에 재배치하기
-    - 첫번째 값이 같거나 가장 높으면 인쇄하기
-        - 이때, 몇번째 인쇄인지 개수를 세는 cnt값 1 올려주기
-        - 만약, 현재 값이 몇번째로 인쇄되었는지 궁금한 문서라면 멈추고 cnt값 프린트하기
+Queue 이용
+- 프린트 조건
+    1. 현재 문서 중요도 확인
+    2. 현재 문서보다 중요도 높은 문서있으면, 현재 문서 뒤로 보냄,
+        현재 문서의 중요도가 가장 높으면, 출력
+- 구해야하는 것: 목표 문서의 출력 순서 (1~)
+- 구현
+    - Queue<int[]>으로 만들어 문서의 id(0~), 중요도를 저장
+    - 현재 문서와 나머지 문서들을 비교했을 때, 큰게 있으면 현재 문서 정보 뒤로 추가(offer)
+    - 현재 문서의 중요도가 가장 높으면, 출력 순서를 높인다.
+         이때, 목표 문서라면, 멈추고 현재 출력 순서 출력
  */
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
@@ -21,34 +26,37 @@ public class Main {
         for (int t = 0; t < T; t++) {
             String[] temp = br.readLine().split(" ");
             int N = Integer.parseInt(temp[0]);
-            int M = Integer.parseInt(temp[1]);
+            int M = Integer.parseInt(temp[1]); // targetId
 
-            temp = br.readLine().split(" ");
-            Queue<int[]> queue = new ArrayDeque<>();
+            Queue<int[]> queue = new ArrayDeque<>(); // id(0~), important
+            temp = br.readLine().split(" "); // docs
+
             for (int i = 0; i < N; i++) {
-                queue.offer(new int[]{Integer.parseInt(temp[i]), i}); // priority, idx
+                int important = Integer.parseInt(temp[i]);
+                queue.offer(new int[]{i, important});
             }
 
-            int cnt = 1;
-            while (!queue.isEmpty()) {
+            int order = 1;
+            while (!queue.isEmpty()) {;
                 int[] cur = queue.poll();
-                boolean isLargest = true;
-                for (int[] other : queue) {
-                    if (cur[0] < other[0]) {
-                        isLargest = false;
+
+                boolean isOk = true;
+                for (int[] q : queue) {
+                    if (cur[1] < q[1]) {
+                        isOk = false;
                         break;
                     }
                 }
-                if (!isLargest) {
+                if (!isOk) {
                     queue.offer(cur);
-                } else {
-                    if (cur[1] == M) {
-                        break;
-                    }
-                    cnt++;
+                    continue;
                 }
+                if (M == cur[0]) {
+                    break;
+                }
+                order++;
             }
-            result.append(cnt).append("\n");
+            result.append(order).append("\n");
         }
 
         bw.write(result.toString());
