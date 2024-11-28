9f2f5bae7a1cf8506c401b68fc7e59a992d6c05c
hsilnam <hsilnam3@gmail.com>
2024-11-28 16:41:33 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 108 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,14 +1,18 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 
 /*
-1. HashMap을 이용하여 옷의 종류별로 갯수를 센다
-2. 각 옷 종류에 대하여 하나씩 뽑히는 경우를 센다
-    - (iC1+1)+(jC1)+...
-    - 여기서 1을 더해주는 것은 아예 그 종류를 안입었을 경우를 뜻함
-3. 마지막에 아무것도 안입은 경우(알몸)을 뺀다
+HashMap 이용
+- type에 맞춰서 종류 카운트
+- 알몸이 아닌 옷들의 조합:
+    - 각 종류마다, 안입은 경우 + 같은 종류의 의상 중 하나 고르는 경우가 있다
+        = (1+iC1)
+    - 의상들의 조합은 곱과 같다
+        = (1+aC1) * (1+bC1) * ...
+    - 전부 안입는 경우가 1개가 나오므로 -1을 한다
+        = (1+aC1) * (1+bC1) * ... -1
  */
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
@@ -16,24 +20,27 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
-        StringBuilder result = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
         for (int t = 0; t < T; t++) {
-            HashMap<String, Integer> cloths = new HashMap<>();
             int N = Integer.parseInt(br.readLine());
+            Map<String, Integer> map = new HashMap<>();
+
             for (int i = 0; i < N; i++) {
                 String[] temp = br.readLine().split(" ");
-                cloths.put(temp[1], cloths.getOrDefault(temp[1], 0) + 1);
+                map.put(temp[1], map.getOrDefault(temp[1], 0) + 1);
             }
 
-            int cnt = 1;
-            for (int cloth : cloths.values()) {
-                cnt *= (cloth + 1);
+            int comb = 1;
+            for (int cnt : map.values()) {
+                comb *= (cnt + 1);
             }
+            comb -= 1;
 
-            result.append(cnt - 1).append("\n");
+            answer.append(comb).append("\n");
         }
 
-        bw.write(result.toString());
+        bw.write(answer.toString());
+
         br.close();
         bw.close();
     }
