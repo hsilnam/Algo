1ff7167668a0c49af1e700e711f0904963730291
hsilnam <hsilnam3@gmail.com>
2024-12-10 11:56:51 +0900

    [Gold III] Title: 게임 닉네임, Time: 412 ms, Memory: 83532 KB -BaekjoonHub

--- 백준/Gold/16934. 게임 닉네임/게임 닉네임.java
+++ 백준/Gold/16934. 게임 닉네임/게임 닉네임.java
@@ -1,51 +1,84 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
+/*
+입력
+- N: 가입한 유저의 수 (1<=N<=100_000)
+- 가입한 닉네임 순서대로
+    - 알파벳 소문자, 길이 10 이하
+
+조건
+- 별칭 Prefix 중 가장 길이가 짧은 것으로
+- prefix 중복 불가능
+- 가능한 별칭이 없는 경우, 가입한 사람의 수 붙이기 (1 제외, 1은 닉네임으로)
+
+
+풀이
+- Trie 활용
+
+
+출력
+- 유저가 가입한 순으로 별칭 한줄 씩 출력
+
+ */
 
 public class Main {
-    static class Trie {
-        Trie[] child;
-        int cnt;
 
-        public Trie() {
-            child = new Trie[26]; // 알파벳 크기
+    public static class Trie {
+        private class TrieNode {
+            TrieNode[] node = new TrieNode[26]; // 알파벳
+            boolean isEnd;
+            int cnt;
         }
 
-        public String insert(String str) {
+        TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        public String add(String s) {
+            TrieNode cur = root;
+            boolean flag = false;
             StringBuilder prefix = new StringBuilder();
-
-            Trie root = this; // 현재 노드부터 시작
-
-            boolean flag = true;
-            for (char c : str.toCharArray()) {
-                if (flag) { // prefix에 추가하기
+            for (char c : s.toCharArray()) {
+                int idx = c - 'a';
+                if(!flag) {
                     prefix.append(c);
                 }
-
-                int alpaIdx = c - 'a'; // 알파벳 위치
-
-                if (root.child[alpaIdx] == null) { // 새로 방문하면 새로 만들기
-                    flag = false; // 새로운 분기점까지만 prefix 처리
-                    root.child[alpaIdx] = new Trie();
+                if (cur.node[idx] == null) {
+                    cur.node[idx] = new TrieNode();
+                    flag = true;
                 }
-                root = root.child[alpaIdx]; // 다음 노드로 가기
+                cur = cur.node[idx];
             }
-            root.cnt += 1;
-            if(root.cnt > 1) {
-                prefix.append(root.cnt);
+            cur.isEnd = true;
+            cur.cnt++;
+            if(cur.cnt > 1) {
+                prefix.append(cur.cnt);
             }
             return prefix.toString();
         }
+
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         Trie trie = new Trie();
-        StringBuilder result = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder answer = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            result.append(trie.insert(br.readLine())).append("\n");
+            String name = br.readLine();
+            answer.append(trie.add(name)).append("\n");
         }
-        System.out.println(result.toString());
+
+        bw.write(answer.toString());
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
