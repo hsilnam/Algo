bc2f4e95bae02170d7a0517090407dc2e7c3ddb1
hsilnam <hsilnam3@gmail.com>
2024-12-16 20:37:57 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 444 ms, Memory: 46844 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,5 +1,33 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+/*
+입력
+- N: 포켓몬 개수 (1<=N<=100_000)
+- M: 맞춰야하는 문제 개수 (1<=M<=100_000)
+- 포켓몬
+    - 1번~
+    - 첫글자만 대문자, 나머지 소문자
+    - 일부는 마지막 문자만 대문자
+    - 2<=길이<=20
+- 문제
+    - 알파벳 ->포켓몬 번호
+    - 숫자 -> 포켓몬 문자
+
+풀이
+- 배열 1번부터 순서대로 포켓몬스터 이름 저장 (N+1크기 배열, 앞에는 패딩)
+- 해쉬맵를 통해 포켓몬이름에 대한 숫자 저장
+- 숫자면 이름, 이름이면 숫자 답하기
+
+출력
+- 각 문제에 대한 답 출력
+
+ */
 
 public class Main {
 
@@ -7,36 +35,32 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] temp = br.readLine().split(" ");
-        int N = Integer.parseInt(temp[0]);
-        int M = Integer.parseInt(temp[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        HashMap<String, Integer> nameInfo = new HashMap<>();
-        HashMap<Integer, String> numInfo = new HashMap<>();
+        String[] names = new String[N + 1];
+        HashMap<String, Integer> numMap = new HashMap<>();
         for (int i = 1; i < N + 1; i++) {
             String name = br.readLine();
-            nameInfo.put(name, i);
-            numInfo.put(i, name);
+            names[i] = name;
+            numMap.put(name, i);
         }
 
-        StringBuilder result = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
         for (int i = 0; i < M; i++) {
-            String input = br.readLine();
-            if(isDigit(input)) {
-                result.append(numInfo.get(Integer.parseInt(input)));
-            } else {
-                result.append(nameInfo.get(input));
+            String quest = br.readLine();
+            if ('A' <= Character.toUpperCase(quest.charAt(0)) && Character.toUpperCase(quest.charAt(0)) <= 'Z') { // 이름
+                answer.append(numMap.get(quest)).append("\n");
+            } else { // 숫자
+                answer.append(names[Integer.parseInt(quest)]).append("\n");
             }
-            result.append("\n");
         }
 
-        bw.write(result.toString());
-
+        bw.write(answer.toString());
+        
         br.close();
+        bw.flush();
         bw.close();
-    }
-
-    public static boolean isDigit(String str) {
-        return ("A".compareTo(str) > 0);
     }
 }
