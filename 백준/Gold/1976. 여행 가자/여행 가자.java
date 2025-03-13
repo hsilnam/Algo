887a7d15368f4e949de4407897e6c8c32faeabdb
hsilnam <hsilnam3@gmail.com>
2025-03-13 14:42:02 +0900

    [Gold IV] Title: 여행 가자, Time: 164 ms, Memory: 17164 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        StringTokenizer st;
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                if (Integer.parseInt(st.nextToken()) == 1) {
+                    union(i, j);
+                }
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int root = find(Integer.parseInt(st.nextToken()));
+        boolean possible = true;
+
+        for (int i = 1; i < M; i++) {
+            if (find(Integer.parseInt(st.nextToken())) != root) {
+                possible = false;
+                break;
+            }
+        }
+
+        bw.write(possible ? "YES" : "NO");
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+        if (rootA != rootB) parent[rootB] = rootA;
+    }
+}
